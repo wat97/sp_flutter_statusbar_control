866874a6dc7dbbeb85450aab19d3dd4acabb64d5
sprin <javad.sharifian@gmail.com>
2023-11-06 12:01:59 +0100

    init

--- example/README.md
+++ example/README.md
@@ -1,9 +1,16 @@
-# Status Bar Control Example
+# example
 
-Demonstrates how to use the `status_bar_control` plugin.
+A new Flutter project.
 
 ## Getting Started
 
-1. Install deps with `flutter pub get`
-2. Start Demo with `flutter run`
-3. Select desired target with 1 - 3
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -109,219 +109,219 @@ class _StatusBarControlAppState extends State<StatusBarControlApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Statusbar Control Example'),
-      ),
-      body: SafeArea(
-        child: Scrollbar(
-          child: ListView(
-            padding:
-                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-            children: <Widget>[
-              renderTitle("Status Bar Height: $_statusBarHeight"),
-              const Divider(height: 25.0),
-              renderTitle("Status Bar Color (Android):"),
-              SwitchListTile(
-                value: _statusBarColorAnimated,
-                title: const Text("Animated:"),
-                onChanged: (bool value) {
-                  setState(() {
-                    _statusBarColorAnimated = value;
-                  });
-                },
-              ),
-              const Text("Color:"),
-              RadioListTile(
-                  value: Colors.black,
-                  title: const Text("Black"),
-                  onChanged: (Color? v) => colorBarChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarColor),
-              RadioListTile(
-                  value: Colors.orange,
-                  title: const Text("Orange"),
-                  onChanged: (Color? v) => colorBarChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarColor),
-              RadioListTile(
-                  value: Colors.greenAccent,
-                  title: const Text("Green"),
-                  onChanged: (Color? v) => colorBarChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarColor),
-              RadioListTile(
-                  value: Colors.white30,
-                  title: const Text("White"),
-                  onChanged: (Color? v) => colorBarChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarColor),
-              const Text("Opacity:"),
-              Slider(
-                value: _statusBarOpacity,
-                min: 0.0,
-                max: 1.0,
-                onChanged: (double val) {
-                  setState(() {
-                    _statusBarOpacity = val;
-                  });
-                  updateStatusBar();
-                },
-              ),
-              const Divider(height: 25.0),
-              renderTitle("Status Bar Hidden:"),
-              SwitchListTile(
-                title: const Text("Hidden:"),
-                value: _statusBarHidden,
-                onChanged: (bool val) {
-                  setState(() {
-                    _statusBarHidden = val;
-                  });
-                  StatusBarControl.setHidden(_statusBarHidden,
-                      animation: _statusBarAnimation);
-                },
-              ),
-              const Text("Animation (iOS):"),
-              RadioListTile(
-                  value: StatusBarAnimation.NONE,
-                  title: const Text("NONE"),
-                  onChanged: (StatusBarAnimation? v) =>
-                      statusBarAnimationChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarAnimation),
-              RadioListTile(
-                  value: StatusBarAnimation.FADE,
-                  title: const Text("FADE"),
-                  onChanged: (StatusBarAnimation? v) =>
-                      statusBarAnimationChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarAnimation),
-              RadioListTile(
-                  value: StatusBarAnimation.SLIDE,
-                  title: const Text("SLIDE"),
-                  onChanged: (StatusBarAnimation? v) =>
-                      statusBarAnimationChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarAnimation),
-              const Divider(height: 25.0),
-              renderTitle("Status Bar Style:"),
-              RadioListTile(
-                  value: StatusBarStyle.DEFAULT,
-                  title: const Text("DEFAULT"),
-                  onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarStyle),
-              RadioListTile(
-                  value: StatusBarStyle.LIGHT_CONTENT,
-                  title: const Text("LIGHT_CONTENT"),
-                  onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarStyle),
-              RadioListTile(
-                  value: StatusBarStyle.DARK_CONTENT,
-                  title: const Text("DARK_CONTENT"),
-                  onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _statusBarStyle),
-              const Divider(height: 25.0),
-              renderTitle("Status Bar Translucent (Android):"),
-              SwitchListTile(
-                title: const Text("Translucent:"),
-                value: _statusBarTranslucent,
-                onChanged: (bool val) {
-                  setState(() {
-                    _statusBarTranslucent = val;
-                  });
-                  StatusBarControl.setTranslucent(_statusBarTranslucent);
-                },
-              ),
-              const Divider(height: 25.0),
-              renderTitle("Status Bar Activity Indicator (iOS):"),
-              SwitchListTile(
-                title: const Text("Indicator:"),
-                value: _loadingIndicator,
-                onChanged: (bool val) {
-                  setState(() {
-                    _loadingIndicator = val;
-                  });
-                  StatusBarControl.setNetworkActivityIndicatorVisible(
-                      _loadingIndicator);
-                },
-              ),
-              const Divider(height: 25.0),
-              renderTitle("Navigation Bar Color (Android):"),
-              SwitchListTile(
-                value: _navBarColorAnimated,
-                title: const Text("Animated:"),
-                onChanged: (bool value) {
-                  setState(() {
-                    _navBarColorAnimated = value;
-                  });
-                },
-              ),
-              const Text("Color:"),
-              RadioListTile(
-                  value: Colors.black,
-                  title: const Text("Black"),
-                  onChanged: (Color? v) => colorNavBarChanged(v!),
-                  dense: true,
-                  groupValue: _navBarColor),
-              RadioListTile(
-                  value: Colors.orange,
-                  title: const Text("Orange"),
-                  onChanged: (Color? v) => colorNavBarChanged(v!),
-                  dense: true,
-                  groupValue: _navBarColor),
-              RadioListTile(
-                  value: Colors.greenAccent,
-                  title: const Text("Green"),
-                  onChanged: (Color? v) => colorNavBarChanged(v!),
-                  dense: true,
-                  groupValue: _navBarColor),
-              RadioListTile(
-                  value: Colors.white12,
-                  title: const Text("white"),
-                  onChanged: (Color? v) => colorNavBarChanged(v!),
-                  dense: true,
-                  groupValue: _navBarColor),
-              const Divider(height: 25.0),
-              renderTitle("Navigation Bar Style (Android):"),
-              RadioListTile(
-                  value: NavigationBarStyle.DEFAULT,
-                  title: const Text("DEFAULT"),
-                  onChanged: (NavigationBarStyle? v) =>
-                      navigationBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _navBarStyle),
-              RadioListTile(
-                  value: NavigationBarStyle.LIGHT,
-                  title: const Text("LIGHT"),
-                  onChanged: (NavigationBarStyle? v) =>
-                      navigationBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _navBarStyle),
-              RadioListTile(
-                  value: NavigationBarStyle.DARK,
-                  title: const Text("DARK"),
-                  onChanged: (NavigationBarStyle? v) =>
-                      navigationBarStyleChanged(v!),
-                  dense: true,
-                  groupValue: _navBarStyle),
-              const Divider(height: 25.0),
-              renderTitle("Fullscreen mode:"),
-              SwitchListTile(
-                title: const Text("Fullscreen:"),
-                value: _fullscreenMode,
-                onChanged: (bool val) {
-                  setState(() {
-                    _fullscreenMode = val;
-                  });
-                  StatusBarControl.setFullscreen(_fullscreenMode);
-                },
-              ),
-            ],
+          appBar: AppBar(
+            title: const Text('Statusbar Control Example'),
           ),
-        ),
-      ),
-    ));
+          body: SafeArea(
+            child: Scrollbar(
+              child: ListView(
+                padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                children: <Widget>[
+                  renderTitle("Status Bar Height: $_statusBarHeight"),
+                  const Divider(height: 25.0),
+                  renderTitle("Status Bar Color (Android):"),
+                  SwitchListTile(
+                    value: _statusBarColorAnimated,
+                    title: const Text("Animated:"),
+                    onChanged: (bool value) {
+                      setState(() {
+                        _statusBarColorAnimated = value;
+                      });
+                    },
+                  ),
+                  const Text("Color:"),
+                  RadioListTile(
+                      value: Colors.black,
+                      title: const Text("Black"),
+                      onChanged: (Color? v) => colorBarChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarColor),
+                  RadioListTile(
+                      value: Colors.orange,
+                      title: const Text("Orange"),
+                      onChanged: (Color? v) => colorBarChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarColor),
+                  RadioListTile(
+                      value: Colors.greenAccent,
+                      title: const Text("Green"),
+                      onChanged: (Color? v) => colorBarChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarColor),
+                  RadioListTile(
+                      value: Colors.white30,
+                      title: const Text("White"),
+                      onChanged: (Color? v) => colorBarChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarColor),
+                  const Text("Opacity:"),
+                  Slider(
+                    value: _statusBarOpacity,
+                    min: 0.0,
+                    max: 1.0,
+                    onChanged: (double val) {
+                      setState(() {
+                        _statusBarOpacity = val;
+                      });
+                      updateStatusBar();
+                    },
+                  ),
+                  const Divider(height: 25.0),
+                  renderTitle("Status Bar Hidden:"),
+                  SwitchListTile(
+                    title: const Text("Hidden:"),
+                    value: _statusBarHidden,
+                    onChanged: (bool val) {
+                      setState(() {
+                        _statusBarHidden = val;
+                      });
+                      StatusBarControl.setHidden(_statusBarHidden,
+                          animation: _statusBarAnimation);
+                    },
+                  ),
+                  const Text("Animation (iOS):"),
+                  RadioListTile(
+                      value: StatusBarAnimation.NONE,
+                      title: const Text("NONE"),
+                      onChanged: (StatusBarAnimation? v) =>
+                          statusBarAnimationChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarAnimation),
+                  RadioListTile(
+                      value: StatusBarAnimation.FADE,
+                      title: const Text("FADE"),
+                      onChanged: (StatusBarAnimation? v) =>
+                          statusBarAnimationChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarAnimation),
+                  RadioListTile(
+                      value: StatusBarAnimation.SLIDE,
+                      title: const Text("SLIDE"),
+                      onChanged: (StatusBarAnimation? v) =>
+                          statusBarAnimationChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarAnimation),
+                  const Divider(height: 25.0),
+                  renderTitle("Status Bar Style:"),
+                  RadioListTile(
+                      value: StatusBarStyle.DEFAULT,
+                      title: const Text("DEFAULT"),
+                      onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarStyle),
+                  RadioListTile(
+                      value: StatusBarStyle.LIGHT_CONTENT,
+                      title: const Text("LIGHT_CONTENT"),
+                      onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarStyle),
+                  RadioListTile(
+                      value: StatusBarStyle.DARK_CONTENT,
+                      title: const Text("DARK_CONTENT"),
+                      onChanged: (StatusBarStyle? v) => statusBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _statusBarStyle),
+                  const Divider(height: 25.0),
+                  renderTitle("Status Bar Translucent (Android):"),
+                  SwitchListTile(
+                    title: const Text("Translucent:"),
+                    value: _statusBarTranslucent,
+                    onChanged: (bool val) {
+                      setState(() {
+                        _statusBarTranslucent = val;
+                      });
+                      StatusBarControl.setTranslucent(_statusBarTranslucent);
+                    },
+                  ),
+                  const Divider(height: 25.0),
+                  renderTitle("Status Bar Activity Indicator (iOS):"),
+                  SwitchListTile(
+                    title: const Text("Indicator:"),
+                    value: _loadingIndicator,
+                    onChanged: (bool val) {
+                      setState(() {
+                        _loadingIndicator = val;
+                      });
+                      StatusBarControl.setNetworkActivityIndicatorVisible(
+                          _loadingIndicator);
+                    },
+                  ),
+                  const Divider(height: 25.0),
+                  renderTitle("Navigation Bar Color (Android):"),
+                  SwitchListTile(
+                    value: _navBarColorAnimated,
+                    title: const Text("Animated:"),
+                    onChanged: (bool value) {
+                      setState(() {
+                        _navBarColorAnimated = value;
+                      });
+                    },
+                  ),
+                  const Text("Color:"),
+                  RadioListTile(
+                      value: Colors.black,
+                      title: const Text("Black"),
+                      onChanged: (Color? v) => colorNavBarChanged(v!),
+                      dense: true,
+                      groupValue: _navBarColor),
+                  RadioListTile(
+                      value: Colors.orange,
+                      title: const Text("Orange"),
+                      onChanged: (Color? v) => colorNavBarChanged(v!),
+                      dense: true,
+                      groupValue: _navBarColor),
+                  RadioListTile(
+                      value: Colors.greenAccent,
+                      title: const Text("Green"),
+                      onChanged: (Color? v) => colorNavBarChanged(v!),
+                      dense: true,
+                      groupValue: _navBarColor),
+                  RadioListTile(
+                      value: Colors.white12,
+                      title: const Text("white"),
+                      onChanged: (Color? v) => colorNavBarChanged(v!),
+                      dense: true,
+                      groupValue: _navBarColor),
+                  const Divider(height: 25.0),
+                  renderTitle("Navigation Bar Style (Android):"),
+                  RadioListTile(
+                      value: NavigationBarStyle.DEFAULT,
+                      title: const Text("DEFAULT"),
+                      onChanged: (NavigationBarStyle? v) =>
+                          navigationBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _navBarStyle),
+                  RadioListTile(
+                      value: NavigationBarStyle.LIGHT,
+                      title: const Text("LIGHT"),
+                      onChanged: (NavigationBarStyle? v) =>
+                          navigationBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _navBarStyle),
+                  RadioListTile(
+                      value: NavigationBarStyle.DARK,
+                      title: const Text("DARK"),
+                      onChanged: (NavigationBarStyle? v) =>
+                          navigationBarStyleChanged(v!),
+                      dense: true,
+                      groupValue: _navBarStyle),
+                  const Divider(height: 25.0),
+                  renderTitle("Fullscreen mode:"),
+                  SwitchListTile(
+                    title: const Text("Fullscreen:"),
+                    value: _fullscreenMode,
+                    onChanged: (bool val) {
+                      setState(() {
+                        _fullscreenMode = val;
+                      });
+                      StatusBarControl.setFullscreen(_fullscreenMode);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
